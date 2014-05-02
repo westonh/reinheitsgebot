be6e06e4ea8f8ecc8a422049810a122ce906f0c3
Weston <whutch@gmail.com>
2014-05-02 12:51:35 -0700

    Update solo.rb

--- solo.rb
+++ solo.rb
@@ -1,5 +1,4 @@
-
-      file_cache_path "/home/ubuntu/chef"
-      cookbook_path "/home/ubuntu/chef/cookbooks"
-      json_attribs "/home/ubuntu/chef/node.json"
+      file_cache_path "/home/westonh/chef"
+      cookbook_path "/home/westonh/chef/cookbooks"
+      json_attribs "/home/westonh/chef/node.json"
     
